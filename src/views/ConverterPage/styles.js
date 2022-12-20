ed019082c3c4d7660313de5c58f122c47d6db9ae
Svitlana <svitlana.ponomarchuk@gmail.com>
2022-12-20 14:53:13 +0200

    added currency converter

---
+++ src/views/ConverterPage/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  padding: 0 calc(50% - 590px);
+  padding-top: 60px;
+  display: flex;
+  justify-content: space-evenly;
+  margin: auto;
+  flex-wrap: wrap;
+  height: 100vh;
+`;
